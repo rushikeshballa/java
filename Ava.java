41e335b48ec941e72332443592e3ef21f0750021
rushikeshballa <rushikeshballa27@gmail.com>
2024-06-28 15:17:37 +0530

    ava

---
+++ Ava.java
@@ -0,0 +1,14 @@
+public class Ava {
+    public static void main(String[] args) {
+       int a=3;
+       System.out.println("a"+ "=" +a);
+       System.out.println( "3*1="+a*1);
+       System.out.println( "3*2="+ a*2); 
+       System.out.println("3*3="+ a*3);
+       System.out.println( "3*4="+a*4);
+       System.out.println( "3*5="+a*5);
+       System.out.println("3*6="+a*6);
+
+    }
+    
+}
